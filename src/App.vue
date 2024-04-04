c23699852fd74336041e75e5bd8786f458999b66
azzyew <laisadnevescosta@gmail.com>
2024-04-04 19:07:07 -0300

    feat: adds intersection observer

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,51 @@
 <script setup lang="ts">
+import { onMounted, ref } from 'vue';
 import HeaderComponent from './components/Header/HeaderComponent.vue';
 import TypewriterAnimation from './components/TypewriterAnimation.vue';
+
+const isOnScreen = ref(false);
+
+onMounted(() => {
+  const observer = new IntersectionObserver(entries => {
+    entries.forEach(entry => {
+      if (entry.isIntersecting) {
+        isOnScreen.value = true;
+      } else {
+        isOnScreen.value = false;
+      }
+    });
+  });
+
+  const target = document.getElementById('#about-me') as HTMLElement;
+
+  observer.observe(target);
+});
+
 </script>
 
 <template>
-  <main class="text-orange-50 h-dvh bg-neutral-900">
+  <main class="text-orange-50 bg-neutral-900 px-16">
     <div class="flex justify-center">
       <HeaderComponent />
     </div>
 
-    <div class="flex flex-col -mt-16 h-[calc(100%_-_2.25rem)] items-center text-center justify-center">
+    <div class="flex flex-col -mt-16 h-[calc(100dvh_-_2.25rem)] items-center text-center justify-center">
       <h1 class="text-4xl font-light tracking-wider mb-4">prazer, sou a Laisa!</h1>
       <TypewriterAnimation display-text="desenvolvedora front-end" />
     </div>
+
+    <div class="p-16 flex flex-col items-center">
+      <h2
+        class="
+          text-2xl text-orange-600 font-light tracking-wider relative w-fit
+          after:bg-orange-600 after:absolute after:h-px after:w-full after:-bottom-1 after:left-0 after:scale-x-0 after:transition-transform after:duration-500  
+        "
+        :class="{ 'animate-fadeIn after:scale-x-100': isOnScreen }"
+        id="#about-me"
+      >quem sou eu</h2>
+      <p class="mt-12">me chamo Laisa, tenho mais de 2.5 anos de experiência e sou apaixonada por desenvolvimento web, principalmente front-end! já trabalhei com diferentes frameworks para aplicações em produção, e tenho sempre muita vontade de aprender e facilidade para isso. levo sempre em consideração acessibilidade, performance e developer experience nos projetos em que participo, e claro user experience acima de tudo.</p>
+    </div>
+
+
   </main>
 </template>
--- tailwind.config.js
+++ tailwind.config.js
@@ -11,11 +11,16 @@ export default {
     extend: {
       animation: {
         'cursor': 'blink 1s step-end infinite',
+        'fadeIn': 'show 1s ease-out forwards',
       },
       keyframes: {
         blink: {
           '0%, 100%': { background: 'transparent' },
           '50%': { backgroundColor: '#9ca3af' },
+        },
+        show: {
+          'from': { opacity: '0' },
+          'to': { opacity: '1' },
         }
       }
     },
